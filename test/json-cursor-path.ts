5630c46bbffdeef5d0dc0e5615094d64e4db9c75
7PH <b.raymond@protonmail.com>
2024-07-08 02:28:50 +0200

    Publish v1.0.0 & Fix ignored non-escaped quotes & Optimize memory usage during parsing & Add prettier

--- test/json-cursor-path.ts
+++ test/json-cursor-path.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
-import { JsonCursorPath } from "../src/json-cursor-path.ts";
+import { JsonCursorPath } from '../src/json-cursor-path.ts';
 
 // Load all json files in fixtures and save them with their file names
 function loadFixtures() {
     const fixtureFiles = fs.readdirSync('./test/fixtures');
     const fixtures = {};
-    fixtureFiles.forEach(file => {
+    fixtureFiles.forEach((file) => {
         fixtures[file] = fs.readFileSync(`./test/fixtures/${file}`, 'utf8');
     });
     return fixtures;
@@ -13,64 +13,89 @@ function loadFixtures() {
 
 const fixtures = loadFixtures();
 
-describe("JsonCursorPath", () => {
+describe('JsonCursorPath', () => {
     describe('objects', () => {
-        describe('simple object', () => {
-            it('string value', () => {
-                const parser = new JsonCursorPath(fixtures['00-object-simple.json'], {
-                    specifyStringIndex: true
-                });
-                const cursor = 20;
-                expect(parser.get(cursor)).toBe('$["first-key"][2]');
-                expect(parser.get(cursor, true)).toEqual([
-                    { type: 'object', key: 'first-key' },
-                    { type: 'string', index: 2 }
-                ])
+        it('string value', () => {
+            const parser = new JsonCursorPath(fixtures['00-object-simple.json'], {
+                specifyStringIndex: true,
             });
-            
-            it('string value in array in object', () => {
-                const parser = new JsonCursorPath(fixtures['00-object-simple.json']);
-                const cursor = 253;
-                expect(parser.get(cursor)).toBe('$["another-object"]["containing-array"][1]');
-                expect(parser.get(cursor, true)).toEqual([
-                    { type: 'object', key: 'another-object' },
-                    { type: 'object', key: 'containing-array' },
-                    { type: 'array', index: 1 },
-                ]);
-            });
-            
-            it('specifying string index', () => {
-                const parser = new JsonCursorPath(fixtures['00-object-simple.json'], {
-                    specifyStringIndex: true,
-                });
-                const cursor = 253;
-                expect(parser.get(cursor)).toBe('$["another-object"]["containing-array"][1][10]');
-                expect(parser.get(cursor, true)).toEqual([
-                    { type: 'object', key: 'another-object' },
-                    { type: 'object', key: 'containing-array' },
-                    { type: 'array', index: 1 },
-                    { type: 'string', index: 10 },
-                ]);
-            });
+            const cursor = 20;
+            expect(parser.get(cursor)).toBe('$["first-key"][2]');
+            expect(parser.get(cursor, true)).toEqual([
+                { type: 'object', key: 'first-key' },
+                { type: 'string', index: 2 },
+            ]);
         });
 
-        describe('jupyter notebook', () => {
-            it('beautify stringified path when key is alphanum', () => {
-                const parser = new JsonCursorPath(fixtures['01-object-jupyter-notebook.json'], {
-                    specifyStringIndex: true,
-                });
-                const cursor = 233331;
-                expect(parser.get(cursor)).toBe('$.cells[21].outputs[1].data["image/png"][23]');
-                expect(parser.get(cursor, true)).toEqual([
-                    { type: 'object', key: 'cells' },
-                    { type: 'array', index: 21 },
-                    { type: 'object', key: 'outputs' },
-                    { type: 'array', index: 1 },
-                    { type: 'object', key: 'data' },
-                    { type: 'object', key: 'image/png' },
-                    { type: 'string', index: 23 },
-                ])
+        it('string value in array in object without returnRawPath', () => {
+            const parser = new JsonCursorPath(fixtures['00-object-simple.json']);
+            const cursor = 253;
+            expect(parser.get(cursor)).toBe('$["another-object"]["containing-array"][1]');
+            expect(parser.get(cursor, true)).toEqual([
+                { type: 'object', key: 'another-object' },
+                { type: 'object', key: 'containing-array' },
+                { type: 'array', index: 1 },
+            ]);
+        });
+
+        it('string value in array in object', () => {
+            const parser = new JsonCursorPath(fixtures['00-object-simple.json'], {
+                specifyStringIndex: true,
             });
+            const cursor = 253;
+            expect(parser.get(cursor)).toBe('$["another-object"]["containing-array"][1][10]');
+            expect(parser.get(cursor, true)).toEqual([
+                { type: 'object', key: 'another-object' },
+                { type: 'object', key: 'containing-array' },
+                { type: 'array', index: 1 },
+                { type: 'string', index: 10 },
+            ]);
+        });
+
+        it('ignore false-flag characters', () => {
+            const parser = new JsonCursorPath(fixtures['01-object-false-flags.json'], {
+                specifyStringIndex: true,
+            });
+            const cursor = 111;
+            expect(parser.get(cursor)).toBe(
+                '$["\\"{}}[]]].:-]\\\\\\\\"][0][0]["\\"{}}[]]].:-]\\\\\\\\"][20]',
+            );
+            expect(parser.get(cursor, true)).toEqual([
+                {
+                    type: 'object',
+                    key: '\\"{}}[]]].:-]\\\\\\\\',
+                },
+                {
+                    type: 'array',
+                    index: 0,
+                },
+                {
+                    type: 'array',
+                    index: 0,
+                },
+                {
+                    type: 'object',
+                    key: '\\"{}}[]]].:-]\\\\\\\\',
+                },
+                { type: 'string', index: 20 },
+            ]);
+        });
+
+        it('beautify stringified path when key is alphanum', () => {
+            const parser = new JsonCursorPath(fixtures['02-object-jupyter-notebook.json'], {
+                specifyStringIndex: true,
+            });
+            const cursor = 233331;
+            expect(parser.get(cursor)).toBe('$.cells[21].outputs[1].data["image/png"][23]');
+            expect(parser.get(cursor, true)).toEqual([
+                { type: 'object', key: 'cells' },
+                { type: 'array', index: 21 },
+                { type: 'object', key: 'outputs' },
+                { type: 'array', index: 1 },
+                { type: 'object', key: 'data' },
+                { type: 'object', key: 'image/png' },
+                { type: 'string', index: 23 },
+            ]);
         });
     });
 });
